b34d5a0f8c95864347f4edcba657d1890eb14706
dahhou ilyas <dahhou.ilyas@gmail.com>
2024-08-05 04:20:53 +0100

    add first commande pong

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -19,6 +19,8 @@ public class Main {
             // Wait for connection from client.
             clientSocket = serverSocket.accept();
 
+            clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         } finally {
