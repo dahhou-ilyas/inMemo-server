be9dd33eebc4b69afc2d0a67a38b11eb2b12145e
dahhou ilyas <dahhou.ilyas@gmail.com>
2024-08-07 23:04:38 +0100

    accepte plusieure commande

--- src/main/java/org/example/ClientHandler.java
+++ src/main/java/org/example/ClientHandler.java
@@ -1,14 +1,10 @@
 package org.example;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
 
 public class ClientHandler implements Runnable{
-
     private Socket clientSocket;
-
     public ClientHandler(Socket socket) {
         this.clientSocket = socket;
     }
@@ -18,6 +14,16 @@ public class ClientHandler implements Runnable{
             InputStream inputStream=clientSocket.getInputStream();
             OutputStream outputStream=clientSocket.getOutputStream();
 
+            BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(inputStream));
+
+            //lire les nomber des line envoyé par commande et a chque ligne envoyé un réponse
+            String inputLine;
+            while ((inputLine=bufferedReader.readLine())!=null){
+                outputStream.write(("+PONG\r\n").getBytes());
+                outputStream.flush();
+            }
+
+            outputStream.write("+PONG\r\n".getBytes());
         }catch (IOException e) {
             System.err.println("Erreur de connexion avec le client : " + e.getMessage());
         } finally {
