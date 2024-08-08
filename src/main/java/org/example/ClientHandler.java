41f5fa4b6ed9d52128a92615fd194fce0e5e1f0d
dahhou ilyas <dahhou.ilyas@gmail.com>
2024-08-08 21:58:07 +0100

    respond to multiple PING commands sent by the same connection

--- src/main/java/org/example/ClientHandler.java
+++ src/main/java/org/example/ClientHandler.java
@@ -3,32 +3,34 @@ package org.example;
 import java.io.*;
 import java.net.Socket;
 
-public class ClientHandler implements Runnable{
+public class ClientHandler implements Runnable {
     private Socket clientSocket;
+
     public ClientHandler(Socket socket) {
         this.clientSocket = socket;
     }
+
     @Override
     public void run() {
-        try {
-            InputStream inputStream=clientSocket.getInputStream();
-            OutputStream outputStream=clientSocket.getOutputStream();
-
-            BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(inputStream));
-
-            //lire les nomber des line envoyé par commande et a chque ligne envoyé un réponse
+        try (
+                InputStream inputStream = clientSocket.getInputStream();
+                OutputStream outputStream = clientSocket.getOutputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))
+        ) {
             String inputLine;
-            while ((inputLine=bufferedReader.readLine())!=null){
-                outputStream.write(("+PONG\r\n").getBytes());
-                outputStream.flush();
+            while ((inputLine = bufferedReader.readLine()) != null) {
+                if (inputLine.toLowerCase().startsWith("ping")) {
+                    outputStream.write(("+PONG\r\n").getBytes());
+                    outputStream.flush();
+                }
             }
 
-        }catch (IOException e) {
+        } catch (IOException e) {
             System.err.println("Erreur de connexion avec le client : " + e.getMessage());
         } finally {
             try {
                 clientSocket.close();
-                System.out.println("connexion fermer");
+                System.out.println("Connexion fermée.");
             } catch (IOException e) {
                 System.err.println("Erreur lors de la fermeture de la socket du client : " + e.getMessage());
             }
